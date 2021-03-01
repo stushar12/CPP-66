48d0e6557b1bd5405a44d1ec8689a28b13049024
Tushar Saxena <stushar345@gmail.com>
2021-03-02 00:55:10 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,10 +1,67 @@
-class Solution{
-    public:
-    int height(struct Node* node)
-    {
-       if(node ==NULL)
-       return 0;
-       else
-       return 1+max(height(node->left),height(node->right));
-    }
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+	int data; 
+	Node* left;
+	Node* right;
 };
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+Node* Insert(Node* root,int data) 
+{
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted is lesser, insert in left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert in right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+int height(Node* root)
+{
+    if(root==NULL)
+    return 0;
+
+    else
+    {
+        return 1 + max(height(root->left),height(root->right));
+    }
+    
+
+}
+
+
+
+
+int main() 
+{
+	Node* root = NULL;  // Creating an empty tree
+
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+    root = Insert(root,6);
+
+    cout<<"Height of given BST is: "<<height(root);
+}
